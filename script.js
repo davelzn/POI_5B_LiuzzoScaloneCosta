96f3d177205bb0f8041608c4f4fa6ada5c2c07d3
DAVIDE <davideliuzzo189@gmail.com>
2025-01-16 22:27:06 +0100

    Aggiunta primi luoghi di prova

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 import { createNavigator } from './navigator.js';
 import { carica, salva, list } from "./carica_salva.js";
-
+import { cTable,cTableAdmin } from './tables.js';
 const navigator = createNavigator(document.querySelector('#container'));
 
-//https://postimg.cc/gallery/QWHJ5L
+//https://postimg.cc/gallery/WFMGy6d
 const apriBtn = document.getElementById("apriBtn");
 const apriLog = document.getElementById("apriLog");
 const loginModal = document.getElementById("loginModal");
 const luoghiModal = document.getElementById("luoghiModal")
 const tableCont = document.getElementById('table-container');
-//componente tabella 
-const cTable = (parentElement, data) => {
-  let html =
-    '<table><thead><tr><th>Name</th><th>Description</th><th>Type</th><th>Average price</th><th>Best season</th><th>Recommended duration</th><th>Family-friendly</th><th>Score</th></tr></thead>';
-  for (let i = 0; i < data.length; i++) {
-    let luogo = data[i];
-    html += `<tr><td>${luogo.nome}</td><td>${luogo.desc}</td><td>${luogo.tipo} ${luogo.att}</td><td>${luogo.per
-      }</td><td>${luogo.tipo}</td></tr>`;
-  }
-  html += '</table>';
-  parentElement.innerHTML = html;
-};
+const tableContAd = document.getElementById("table-container-ad");
+const homeBtn = document.getElementById("home-btn-ad");
+const homeBtnSp = document.getElementById("home-btn-sp");
+const titleModal = document.getElementById("modal-title");
+const addPlaceBtn = document.getElementById("add-btn-ad");
+const headerAd = document.getElementById("header-ad");
 
 // inizializzazione mappa
-let zoom = 5;
+let zoom = 4;
 let maxZoom = 19;
 let map = L.map('map').setView([-27.3585804, 132.5606708], zoom);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -34,7 +28,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 
-let tokenMap,myToken;
+let tokenMap, myToken;
 fetch('./conf.json') // carica le variabili da conf.json
   .then((response) => {
     if (!response.ok) {
@@ -44,142 +38,169 @@ fetch('./conf.json') // carica le variabili da conf.json
   })
   .then((data) => {
     tokenMap = data.tokenMap;
-    myToken=data.cacheToken;
+    myToken = data.cacheToken;
     console.log(tokenMap);
     carica().then(() => {
       console.log(list);
       renderLuoghi();
       cTable(tableCont, list);
+      cTableAdmin(tableContAd, list)
     });
   })
   .catch((error) => console.error('Errore:', error));
 
-//Bottoni della modale
-document.getElementById("chiudiBtn").onclick = () => {
-   luoghiModal.style.display = 'none';
-};
-document.getElementById("submit").onclick = () => {
-    SubmForm();
-}
+
+
+
+
+
 
 //funzione per inviare i dati del form
 function SubmForm() {
-  let nome = document.getElementById('address').value;
+  let nome = document.getElementById('name').value;
+  let desc = document.getElementById('desc').value;
+  let foto = document.getElementById('foto').value.split(',');
+  let per = document.getElementById('per').value;
+  let tipo = document.getElementById('tipo').value;
+  let att = document.getElementById('att').value;
+  let prz = document.getElementById('prz').value;
+  let dur = document.getElementById('dur').value;
+  let ff = document.getElementById('ff').value;
+  let vic = document.getElementById('vic').value;
+  let punt = document.getElementById('punt').value;
+
   const esitoDiv = document.getElementById('esito');
-  let indAU = nome + ', Australia';
-  let url = `https://us1.locationiq.com/v1/search?key=${tokenMap}&q=${encodeURIComponent(
-    indAU)}&format=json`;
-  if (!nome || !foto || !per || !tipo) {
+
+  if (!nome || !foto || !per || !tipo || !att || !prz || !dur || !ff || !vic || !punt) {
     esitoDiv.innerHTML = '<div class="alert alert-danger">Compila tutti i campi!</div>';
     return;
-}
+  }
+
+  let indAU = nome + ', Australia';
+  let url = `https://us1.locationiq.com/v1/search?key=${tokenMap}&q=${encodeURIComponent(indAU)}&format=json`;
+
   fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+    .then(response => response.json())
+    .then(data => {
       if (data.error === 'Unable to geocode') {
-        esitoDiv.innerHTML =
-          '<div class="alert alert-danger">Nome non valido o inesistente!</div>';
+        esitoDiv.innerHTML = '<div class="alert alert-danger">Nome non valido o inesistente!</div>';
       } else {
-        let desc = document.getElementById('desc').value.split(',');
-        let foto = document.getElementById('foto').value.split(',');
-        let per = document.getElementById('per').value;
-        let tipo = document.getElementById('tipo').value;
-        let att = document.getElementById('att').value;
-        let prz = document.getElementById('prz').value;
-        let dur = document.getElementById('dur').value;
-        let coord = document.getElementById('cord').value.split(',');
-        let ff = document.getElementById('ff').value;
-        let vic = document.getElementById('vic').value;
-        let punt = document.getElementById('punt').value;
-        let lat = coord[0]
-        let lon = coord[1]
+        let lat = data[0].lat;
+        let lon = data[0].lon;
+
         const k = {
           nome: indAU,
           desc: desc,
           foto: foto,
           per: per,
           tipo: tipo,
-          att : att,
-          prz : prz,
-          dur : dur,
-          lat : lat,
+          att: att,
+          prz: prz,
+          dur: dur,
+          lat: lat,
           lon: lon,
-          ff : ff,
+          ff: ff,
           vic: vic,
           punt: punt
         };
-        console.log(list);
+
         list.push(k);
-        console.log(list);
+
         salva().then(() => {
           renderLuoghi();
         });
 
-        esitoDiv.innerHTML ='<div class="alert alert-success">Prenotazione aggiunta con successo!</div>';
-        //reset dei campi
-        document.getElementById('address').value = '';
+        esitoDiv.innerHTML = '<div class="alert alert-success">Prenotazione aggiunta con successo!</div>';
+
+        // Reset dei campi
+        document.getElementById('name').value = '';
         document.getElementById('desc').value = '';
         document.getElementById('foto').value = '';
         document.getElementById('per').value = '';
         document.getElementById('tipo').value = '';
-        document.getElementById('luoghiModal').style.display = 'none';
-        document.body.classList.remove('modal-open'); 
-        document.querySelector('.modal-backdrop').remove(); 
+        document.getElementById('att').value = '';
+        document.getElementById('prz').value = '';
+        document.getElementById('dur').value = '';
+        document.getElementById('ff').value = '';
+        document.getElementById('vic').value = '';
+        document.getElementById('punt').value = '';
+
+        // Chiusura modale
+        luoghiModal.style.display = 'none';
+        document.body.classList.remove('modal-open');
+        const backdrop = document.querySelector('.modal-backdrop');
+        if (backdrop) {
+          backdrop.remove();
+        }
 
         cTable(tableCont, list);
       }
     })
-    .catch((error) => {
+    .catch(error => {
       console.error("Errore durante il controllo dell'nome:", error);
-      esitoDiv.innerHTML =
-        '<div class="alert alert-danger">Si è verificato un errore durante la verifica dell\'nome!</div>';
+      esitoDiv.innerHTML = '<div class="alert alert-danger">Si è verificato un errore durante la verifica dell\'nome!</div>';
     });
 }
 
 
 
-function renderLuoghi() {
-    for (let i = 0; i < list.length; i++) {
-      let luogo = list[i];
-      const coords = [luogo.lat, luogo.lon];
-      const marker = L.marker(coords).addTo(map);
-      const popupContent = `
-        <b>Nome:</b> ${luogo.nome}<br>
-        <b>:</b> ${luogo.desc}<br>
-        <b>Data e Ora:</b> ${luogo.foto[0]} ${
-        luogo.foto[1]
-      }<br>
-        <b>per:</b> ${luogo.per}<br>
-        <b>tipo:</b> ${luogo.tipo}
-      `;
-      marker.bindPopup(popupContent).openPopup();
-}
-  }
 
-  
-  const filterBtn = document.getElementById('filter-btn');
-  const resetBtn = document.getElementById('reset-btn');
-  
-  filterBtn.onclick = () => {
-    console.log('Funzione filtro');
-    let ind1 = document.getElementById('filtro').value;
-    console.log(ind1);
-    let filteredList = list.filter((k) =>
-      k.nome.toLowerCase().includes(ind1.toLowerCase())
-    );
-    console.log(filteredList);
-    cTable(tableCont, filteredList);
-    document.getElementById('filtro').value = '';
-  };
-  resetBtn.onclick = () => {
-    cTable(tableCont, list);
-  };
+function renderLuoghi() {
+  for (let i = 0; i < list.length; i++) {
+    let luogo = list[i];
+    const coords = [luogo.lat, luogo.lon];
+    const marker = L.marker(coords).addTo(map);
+    const popupContent = `
+        <b>Nome:</b> ${luogo.nome}<br>
+        <b>Description:</b> ${luogo.desc}<br>
+        <b>Type:</b> ${luogo.tipo}<br>
+        <b>Main activities:</b> ${luogo.att}<br>
+        <b>Best Season:</b> ${luogo.per}<br>
+        <b>Recommended Duration:</b> ${luogo.dur}<br>
+        <b>Family-Friendly:</b> ${luogo.ff}<br>
+      `;
+    marker.bindPopup(popupContent).openPopup();
+  }
+}
+
+//GESTIONE BOTTONI
+const filterBtn = document.getElementById('filter-btn');
+const resetBtn = document.getElementById('reset-btn');
+
+document.getElementById("submit").onclick = () => {
+  SubmForm();
+}
+
+filterBtn.onclick = () => {
+  console.log('Funzione filtro');
+  let ind1 = document.getElementById('filtro').value;
+  console.log(ind1);
+  let filteredList = list.filter((k) =>
+    k.nome.toLowerCase().includes(ind1.toLowerCase())
+  );
+  console.log(filteredList);
+  cTable(tableCont, filteredList);
+  document.getElementById('filtro').value = '';
+};
+resetBtn.onclick = () => {
+  cTable(tableCont, list);
+};
+
+addPlaceBtn.onclick = () => {
+  titleModal.innerHTML = "Add Place"
+}
+
+homeBtn.onclick = () => {
+  window.location.hash = 'pagina1';
+}
+homeBtnSp.onclick = () => {
+  window.location.hash = 'pagina1';
+}
 const createLogin = () => {
   const inputName = document.querySelector("#user");
   const inputPassword = document.querySelector("#psw");
   const loginButton = document.getElementById("loginBtn");
-  const esitoLog = document.getElementById("esitoLog");  
+  const esitoLog = document.getElementById("esitoLog");
   let isLogged = false;
 
   // Funzione login
@@ -218,6 +239,8 @@ const createLogin = () => {
           backdrop.remove();
         }
         window.location.hash = 'pagina2';
+        headerAd.classList.remove = "hidden"
+        cTableAdmin(tableContAd,list)
       } else {
         esitoLog.innerHTML =
           '<div class="alert alert-danger">Credenziali Errate!</div>';
---
+++ tables.js
@@ -0,0 +1,45 @@
+//COMPONENTE TABELLA
+export const cTable = (parentElement, data) => {
+    let html =
+        '<table><thead><tr><th>Name</th><th>Description</th><th>Type</th><th>Average price</th><th>Best season</th><th>Recommended duration</th><th>Family-friendly</th><th>Score</th></tr></thead>';
+    for (let i = 0; i < data.length; i++) {
+        let luogo = data[i];
+        html += `<tr>
+                    <td>${luogo.nome}</td>
+                    <td>${luogo.desc}</td>
+                    <td>${luogo.tipo}</td>
+                    <td>${luogo.prz}</td>
+                    <td>${luogo.per}</td>
+                    <td>${luogo.dur}</td>
+                    <td>${luogo.ff}</td>
+                    <td>${luogo.punt}</td>
+                 </tr>`;
+    }
+    html += '</table>';
+    parentElement.innerHTML = html;
+};
+
+export const cTableAdmin = (parentElement, data) => {
+    let html =
+        '<table class="table-ad"><thead><tr><th>Name</th><th>Description</th><th>Type</th><th>Average price</th><th>Best season</th><th>Recommended duration</th><th>Coordinates</th><th>Family-friendly</th><th>Close to</th><th>Score</th><th>Edit</th><th>Delete</th></tr></thead>';
+    for (let i = 0; i < data.length; i++) {
+        let luogo = data[i];
+        let coordinates = `${luogo.lat}, ${luogo.lon}`;
+        html += `<tr>
+                    <td>${luogo.nome}</td>
+                    <td>${luogo.desc}</td>
+                    <td>${luogo.tipo}</td>
+                    <td>${luogo.prz}</td>
+                    <td>${luogo.per}</td>
+                    <td>${luogo.dur}</td>
+                    <td>${coordinates}</td>
+                    <td>${luogo.ff}</td>
+                    <td>${luogo.vic}</td>
+                    <td>${luogo.punt}</td>
+                    <td><button id="editBtn${i}" class="edit-btn">Edit</button></td>
+                    <td><button id="delBtn${i}" class="del-btn">Delete</button></td>
+                 </tr>`;
+    }
+    html += '</table>';
+    parentElement.innerHTML = html;
+};
